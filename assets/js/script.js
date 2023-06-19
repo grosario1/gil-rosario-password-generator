f268d3309535dbaa0d9313afd12f440c9554d550
Gil Rosario <grosario@alum.quinnipiac.edu>
2023-06-19 16:37:02 -0400

    added code to prompt for selection of types of characters

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,7 +30,8 @@ generateBtn.addEventListener("click", writePassword);
         return password;
       }
 
-// Prompt for password length choosing a number between 8 and 128 characters. User must choose or an Invalid message is prompted
+// This function will prompt for password length choosing a 
+//number between 8 and 128 characters. User must choose or an Invalid message is prompted
 function getPasswordLength() {
   var length = parseInt(prompt("Enter the length of the password (between 8 and 128 characters):"));
 
@@ -39,4 +40,28 @@ function getPasswordLength() {
   }
 
   return length;
+}
+
+// This function will prompt for character types and confirm selection 
+function getCharacterSet() {
+  var lowercase = confirm("Include lowercase characters?");
+  var uppercase = confirm("Include uppercase characters?");
+  var numeric = confirm("Include numeric characters?");
+  var special = confirm("Include special characters?");
+
+  while (!(lowercase || uppercase || numeric || special)) {
+    alert("At least one character type must be selected!");
+    lowercase = confirm("Include lowercase characters?");
+    uppercase = confirm("Include uppercase characters?");
+    numeric = confirm("Include numeric characters?");
+    special = confirm("Include special characters?");
+  }
+
+  var charSet = "";
+  if (lowercase) charSet += "abcdefghijklmnopqrstuvwxyz";
+  if (uppercase) charSet += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  if (numeric) charSet += "0123456789";
+  if (special) charSet += "!@#$%^&*()";
+
+  return charSet;
 }
